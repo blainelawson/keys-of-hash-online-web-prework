f7896ab60da41eb3a935b13a092a855aa1847564
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-19 15:19:31 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     array = Array.new
     
-    return array = ["red-footed tortoise"] if arguments == "Panama"
+    return array = [*"red-footed tortoise"] if arguments == "Panama"
     return array = []
 
   end
