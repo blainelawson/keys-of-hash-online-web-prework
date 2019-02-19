923837dce2c5dde137fd5474a5367dc8fa72bd1e
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-19 15:38:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,10 @@ require "pry"
 
 class Hash
   def keys_of(*arguments)
-    binding.pry
-    array = Array.new
     
-    return array = ["red-footed tortoise"] if arguments == "Panama"
-    return array = [] if arguments == "Madagascar"
+    
+    self.map do |key, values|
+      binding.pry  
+    end
   end
 end
